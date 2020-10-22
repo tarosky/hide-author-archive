fa37253ecdb9ebabe6f745707d48044422595985
fumikito <guy@hametuha.com>
2020-10-22 14:37:33 +0900

    Fix workflow file.

--- bin/build.sh
+++ bin/build.sh
@@ -14,20 +14,3 @@ sed -i "s/%nightly%/$VERSION/" hide-author-archive.php
 
 # Generate README
 curl -L https://raw.githubusercontent.com/fumikito/wp-readme/master/wp-readme.php | php
-
-# Remove unwanted files
-echo "Remove unwanted files.";
-rm -rf ./.git
-rm -rf ./.github
-rm -rf ./bin
-rm -rf ./tests
-rm -rf ./vendor
-rm -rf ./wp
-rm -rf ./.gitattributes
-rm -rf ./.gitignore
-rm -rf ./.wp-env.json
-rm -rf ./composer.lock
-rm -rf ./package-lock.json
-rm -rf ./phpunit.xml.dist
-rm -rf ./README.md
-rm -rf ./.wordpress-org
